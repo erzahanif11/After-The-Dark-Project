d22e9f588a77c7f1fcb85a1facc99059a6e98c0f
CrimsonsTempest <rafaeldevenang@gmail.com>
2025-03-04 15:43:21 +0700

    PlayerMovement base
    dasar banget movement

---
+++ After The Dark/Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    private Vector2 movement;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.A))
+            move.x -= moveSpeed * Time.deltaTime; // Move left
+        if (Input.GetKey(KeyCode.D))
+            move.x += moveSpeed * Time.deltaTime; // Move right
+        if (Input.GetKey(KeyCode.W))
+            move.z += moveSpeed * Time.deltaTime; // Move forward
+        if (Input.GetKey(KeyCode.S))
+            move.z -= moveSpeed * Time.deltaTime; // Move backward
+
+        transform.position += move;
+
+    }
+
+    
+
+}
