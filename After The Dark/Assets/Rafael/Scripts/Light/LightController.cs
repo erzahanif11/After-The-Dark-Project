6856398936260d866f641e540a890fc31a832b76
erzahanif11 <erzahanif11@gmail.com>
2025-03-10 19:18:57 +0700

    Add Room Light
    Room light only turn on in certain times

---
+++ After The Dark/Assets/Rafael/Scripts/Light/LightController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public float lightOnDuration = 120f; // 2 menit
+    private float timer;
+    private Light[] roomLights;
+
+    private void Start()
+    {
+        roomLights = GameObject.FindGameObjectsWithTag("RoomLight")
+                        .Select(obj => obj.GetComponent<Light>())
+                        .ToArray();
+        timer = lightOnDuration;
+        ToggleLights(true);
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            ToggleLights(false);
+            //StartCoroutine(TurnOnLightsAfterDelay(1f));
+            timer = lightOnDuration;
+        }
+    }
+
+    private void ToggleLights(bool state)
+    {
+        foreach (Light light in roomLights)
+        {
+            if (light != null)
+                light.enabled = state;
+        }
+    }
+
+    private IEnumerator TurnOnLightsAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        ToggleLights(true);
+    }
+}
